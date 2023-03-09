bab66a03dd9eabcb415b5d7cb91dcbad2e394059
ignaciansilva <ignacioansilva@gmail.com>
2023-03-09 20:16:53 -0300

    reveal uninstalled

--- src/App.js
+++ src/App.js
@@ -2,29 +2,23 @@ import './App.css';
 import Footer from "./Components/Footer"
 import SectionGetInTouch from './Components/Section-1';
 import DiagonalSection from './Components/diagonal-section';
-import Fade from 'react-reveal/Fade';
-import Zoom from 'react-reveal/Zoom';
 import Card from './Components/cards';
 import ContactMe from './Components/Contact-me';
 
 function App() {
   return (
     <div className="App">
-      <Zoom>
+     
       <DiagonalSection />
-      </Zoom>
+     
       <br/>
-      <Zoom>
+     
       <SectionGetInTouch />
-      </Zoom>
-      <Fade right>
       
       <Card />
-      
-      </Fade>
-      <Zoom>
+   
       <ContactMe />
-      </Zoom>
+    
      <Footer />
     </div>
   );
